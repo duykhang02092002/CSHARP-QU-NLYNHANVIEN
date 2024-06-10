150503456843de81e1f2463ea9bc9594f81a66df
ADMIN <ADMIN@LAB1-MAY20>
2024-06-10 15:07:56 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CSHARP_QUẢNLYNHANVIEN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTHEM = new System.Windows.Forms.Button();
+            this.txtso = new System.Windows.Forms.TextBox();
+            this.lbsDANHSACH = new System.Windows.Forms.ListBox();
+            this.btnXOA = new System.Windows.Forms.Button();
+            this.btnSUA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(220, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(333, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "NHẬP HỌ TÊN NHÂN VIÊN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Matura MT Script Capitals", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(655, 50);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "QUẢN LÝ NHÂN VIÊN";
+            // 
+            // btnTHEM
+            // 
+            this.btnTHEM.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTHEM.Location = new System.Drawing.Point(430, 162);
+            this.btnTHEM.Name = "btnTHEM";
+            this.btnTHEM.Size = new System.Drawing.Size(65, 33);
+            this.btnTHEM.TabIndex = 6;
+            this.btnTHEM.Text = "THÊM";
+            this.btnTHEM.UseVisualStyleBackColor = true;
+            this.btnTHEM.Click += new System.EventHandler(this.btnTHEM_Click);
+            // 
+            // txtso
+            // 
+            this.txtso.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtso.Location = new System.Drawing.Point(174, 157);
+            this.txtso.Name = "txtso";
+            this.txtso.Size = new System.Drawing.Size(237, 38);
+            this.txtso.TabIndex = 5;
+            // 
+            // lbsDANHSACH
+            // 
+            this.lbsDANHSACH.FormattingEnabled = true;
+            this.lbsDANHSACH.Location = new System.Drawing.Point(174, 216);
+            this.lbsDANHSACH.Name = "lbsDANHSACH";
+            this.lbsDANHSACH.Size = new System.Drawing.Size(464, 186);
+            this.lbsDANHSACH.TabIndex = 4;
+            // 
+            // btnXOA
+            // 
+            this.btnXOA.Location = new System.Drawing.Point(501, 162);
+            this.btnXOA.Name = "btnXOA";
+            this.btnXOA.Size = new System.Drawing.Size(69, 33);
+            this.btnXOA.TabIndex = 9;
+            this.btnXOA.Text = "XÓA";
+            this.btnXOA.UseVisualStyleBackColor = true;
+            this.btnXOA.Click += new System.EventHandler(this.btnXOA_Click);
+            // 
+            // btnSUA
+            // 
+            this.btnSUA.Location = new System.Drawing.Point(576, 162);
+            this.btnSUA.Name = "btnSUA";
+            this.btnSUA.Size = new System.Drawing.Size(62, 33);
+            this.btnSUA.TabIndex = 9;
+            this.btnSUA.Text = "SỬA";
+            this.btnSUA.UseVisualStyleBackColor = true;
+            this.btnSUA.Click += new System.EventHandler(this.btnSUA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSUA);
+            this.Controls.Add(this.btnXOA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTHEM);
+            this.Controls.Add(this.txtso);
+            this.Controls.Add(this.lbsDANHSACH);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTHEM;
+        private System.Windows.Forms.TextBox txtso;
+        private System.Windows.Forms.ListBox lbsDANHSACH;
+        private System.Windows.Forms.Button btnXOA;
+        private System.Windows.Forms.Button btnSUA;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSHARP_QUẢNLYNHANVIEN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTHEM_Click(object sender, EventArgs e)
+        {
+            lbsDANHSACH.Items.Add(txtso.Text);
+
+        }
+
+        private void btnXOA_Click(object sender, EventArgs e)
+        {
+            lbsDANHSACH.Items.RemoveAt(lbsDANHSACH.SelectedIndex);
+        }
+
+        private void btnSUA_Click(object sender, EventArgs e)
+        {
+            if (lbsDANHSACH.SelectedIndex < 0) return;
+            lbsDANHSACH.Items[lbsDANHSACH.SelectedIndex] = txtso.Text;
+        }
+    }
+}
